ea7c0dabf1d9cb5173d817b7611adb8bb6461a51
Luc FERRO <luchenri.ferro@gmail.com>
2025-01-25 01:43:25 +0100

    first commit

---
+++ Assets/CameraControls.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class CameraControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float horizontalSensitivity = 1f;
+    public float verticalSensitivity = 1f;
+    private Transform camTransform;
+    private Camera cam;
+    private float xMovement;
+    private float yMovement;
+    private Vector3 camRotation;
+    private Vector3 startingRotation;
+    void Start()
+    {
+        camTransform = GetComponentInChildren<Transform>();
+        cam = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        startingRotation = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMovement = Input.GetAxisRaw("Mouse X") * horizontalSensitivity;
+        yMovement = -Input.GetAxisRaw("Mouse Y") * verticalSensitivity;
+        yMovement = 0f;
+
+        camRotation = new Vector3(Mathf.Clamp(camRotation.x + yMovement, -90, 90), camRotation.y + xMovement, camRotation.z);
+
+        if (Input.GetMouseButton(0))
+        {
+            camTransform.eulerAngles = camRotation;
+        }
+        else
+        {
+            transform.eulerAngles = camRotation;
+        }
+
+    }
+}
