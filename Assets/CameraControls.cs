15b7d7bb32c33775a4b96bb58733d48c88ed4e5c
Luc FERRO <luchenri.ferro@gmail.com>
2025-01-31 03:39:12 +0100

    camera controls WIP

--- Assets/CameraControls.cs
+++ Assets/CameraControls.cs
@@ -10,13 +10,15 @@ public class CameraControls : MonoBehaviour
     public float verticalSensitivity = 1f;
     private Transform camTransform;
     private Camera cam;
+    private float movement;
     private float xMovement;
     private float yMovement;
+    private Vector3 camHolderRotation;
     private Vector3 camRotation;
     private Vector3 startingRotation;
     void Start()
     {
-        camTransform = GetComponentInChildren<Transform>();
+        //camTransform = GetComponentInChildren<Transform>();
         cam = GetComponentInChildren<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -26,12 +28,14 @@ public class CameraControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        movement = -Input.GetAxisRaw("Horizontal");
         xMovement = Input.GetAxisRaw("Mouse X") * horizontalSensitivity;
         yMovement = -Input.GetAxisRaw("Mouse Y") * verticalSensitivity;
-        yMovement = 0f;
 
+        camHolderRotation = new Vector3(Mathf.Clamp(camHolderRotation.x, -90, 90), Mathf.Clamp(camHolderRotation.y + movement, -92, 27) , camHolderRotation.z);
         camRotation = new Vector3(Mathf.Clamp(camRotation.x + yMovement, -90, 90), camRotation.y + xMovement, camRotation.z);
 
-        transform.eulerAngles = camRotation;
+        cam.transform.eulerAngles = camRotation;
+        transform.eulerAngles = camHolderRotation;
     }
 }
